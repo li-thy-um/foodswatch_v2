b56ce14349444515b36dd5cccfd99317c30a1b1e
lithium <lithium4010@163.com>
2013-12-07 13:33:30 +0800

    finish signin

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -44,3 +44,15 @@ def user_pages
     "user_path(user)" => UserPageInfo.new("Profile page", USER.name, USER.name,
                                           { full_title: false }, USER) }
 end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password 
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text:message)
+  end
+end
