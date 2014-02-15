86e0b59b473594e342fa0ee2347f30e18af95660
lithium <lithium4010@163.com>
2014-02-15 20:57:28 +0800

    fix migrate file

--- db/migrate/20140213124435_add_column_watcher_count_post_food_id.rb
+++ db/migrate/20140213124435_add_column_watcher_count_post_food_id.rb
@@ -1,4 +1,4 @@
-class AddColumnWatcherCountPostFoodIdCommentCount < ActiveRecord::Migration
+class AddColumnWatcherCountPostFoodId < ActiveRecord::Migration
   def change
     add_column :watches, :watcher_count, :integer
     add_column :microposts, :post_food_id, :integer, default: 0
