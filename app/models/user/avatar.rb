b9fd370d4b490693ae3271f4e38216963ff37fca
li-thy-um <lithium4010@163.com>
2015-05-18 13:55:20 +0800

    refactor

--- app/models/user/avatar.rb
+++ app/models/user/avatar.rb
@@ -20,47 +20,38 @@ module User::Avatar
 
   # 上传头像
   def upload_avatar(avatar_file)
-
     validate_file_size avatar_file
-
     file_name = next_file_name
-
     put_policy = Qiniu::Auth::PutPolicy.new(
       QINIU_BUKET,     # 存储空间
       file_name        # 最终资源名，可省略
     )
-
     uptoken = Qiniu::Auth.generate_uptoken(put_policy)
-
     post_data = {
       file: avatar_file,
       token: uptoken,
       key: file_name
     }
-
     ### 发送请求
     response = RestClient.post(QINIU_UPLOAD_URL, post_data)
-
-    raise "Avatar Upload Post request fail, post_data: #{post_data.inspect}. response: #{response.inspect}" unless response.code == 200
-
-    remove_avatar_file if !avatar.nil?
-
+    unless response.code == 200
+      raise "Avatar Upload Post request fail, post_data: #{post_data.inspect}. response: #{response.inspect}"
+    end
+    remove_avatar_file! if avatar?
     self.update_attribute :avatar, file_name
   end
 
   def remove_avatar!
-    return if avatar.nil?
-    remove_avatar_file
+    return unless avatar?
+    remove_avatar_file!
     update_attribute :avatar, nil
   end
 
-  def remove_avatar_file
-
+  def remove_avatar_file!
     code, result, response_headers = Qiniu::Storage.delete(
       QINIU_BUKET, # 存储空间
       avatar    # 资源名
     )
-
     raise "Avatar Delete Post request fail, result: #{result}" unless code == 200
   end
 
@@ -71,7 +62,9 @@ module User::Avatar
   private
 
   def validate_file_size(avatar_file)
-    raise "Avatar File Too Big, file_size: #{avatar_file.size}, max_size: #{MAX_AVATAR_SIZE}" if avatar_file.size > MAX_AVATAR_SIZE
+    if avatar_file.size > MAX_AVATAR_SIZE
+      raise "Avatar File Too Big, file_size: #{avatar_file.size}, max_size: #{MAX_AVATAR_SIZE}"
+    end
   end
 
   def next_file_name
