54cf807a414dcae968ec64391a821e9eafbf5d92
li-thy-um <lithium4010@163.com>
2015-05-18 14:21:15 +0800

    add 'ctrl+enter' to post share and comment

--- app/assets/javascripts/micropost_form.js
+++ app/assets/javascripts/micropost_form.js
@@ -1,12 +1,12 @@
 $(init_post_form);
 
 function init_post_form(){
-  $("body").on('keydown',  '#new_post_textarea', submit_form);
+  $(document).on('keydown', '#new_post_textarea, .comment-text-area, .share-text-area', submit_form);
 }
 
 var submit_form = function(e) {
   //When ctrl+enter pressed, submit the form.
   if(e.ctrlKey && e.which == "13") {
-    $("#new_post_form").submit();
+    $(this).closest('form').submit();
   }
 }
